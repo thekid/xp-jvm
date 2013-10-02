7ad2be9780def6143bebb9a1c1c1d680c28312e0
Timm Friebe <thekid@thekid.de>
2013-10-02 09:10:22 +0200

    - QA: WS

--- HelloWorld.php
+++ HelloWorld.php
@@ -1,8 +1,7 @@
 <?php
-
 class HelloWorld extends \lang\Object {
   public static function main($args) {
-  	if (sizeof($args) > 0) {
+    if (sizeof($args) > 0) {
       \util\cmd\Console::writeLine('Hello ', $args[0]);
     } else {
       \util\cmd\Console::writeLine('Hello World');
